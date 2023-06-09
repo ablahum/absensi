b446dde7ac6af9688ba815c0559519b26366ec1f
ablahum <mriztama@gmail.com>
2023-06-09 23:31:34 +0700

    refacto

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="./styles/style.css"/>
+    <!-- <link rel="stylesheet" href="./styles/style.css"/> -->
     <title>Absensi</title>
   </head>
-  
+
   <body>
     <div class="container">
       <div  class="wrapper">
